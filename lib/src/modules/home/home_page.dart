5a4ad19512913340e9ba08da2ac6f09983e29b2e
John Rocha <johnathanrocha@gmail.com>
2023-05-10 09:49:57 -0300

    feat: messages

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../core/env/env.dart';
 import '../../core/ui/helpers/loader.dart';
+import '../../core/ui/helpers/messages.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,7 +11,7 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with Loader {
+class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +26,7 @@ class _HomePageState extends State<HomePage> with Loader {
           showLoader();
           await Future.delayed(const Duration(seconds: 2));
           hideLoader();
+          showError('Erro de alguma coisa');
         },
       ),
     );
