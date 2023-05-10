3cc980001dc1cfb05b3577a0ef8c3286e9c48c5f
John Rocha <johnathanrocha@gmail.com>
2023-05-10 09:37:56 -0300

    feat: loader

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 import '../../core/env/env.dart';
+import '../../core/ui/helpers/loader.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +19,13 @@ class HomePage extends StatelessWidget {
       ),
       body: Container(
         child: Text(Env.instance.get('backend_base_url')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          showLoader();
+          await Future.delayed(const Duration(seconds: 2));
+          hideLoader();
+        },
       ),
     );
   }
